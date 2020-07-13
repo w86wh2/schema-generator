9c837ef318f6c172c2c77bf9709bb6a45102f9bf
FateRiddle <fateriddle@gmail.com>
2020-07-13 20:38:47 +0800

    跟新配置

--- .umirc.js
+++ .umirc.js
@@ -8,5 +8,8 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'doc',
+  base: '/schema-generator/',
+  publicPath: '/schema-generator/',
+  exportStatic: {},
   // more config: https://d.umijs.org/config
 });
