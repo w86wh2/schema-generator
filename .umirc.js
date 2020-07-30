7c9f7a17a157cbc8b5fc28550490dc0a5f839518
FateRiddle <fateriddle@gmail.com>
2020-07-30 14:34:56 +0800

    添加到github和changelog的link

--- .umirc.js
+++ .umirc.js
@@ -6,6 +6,18 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  navs: [
+    null,
+    {
+      title: 'GitHub',
+      path: 'https://github.com/form-render/schema-generator',
+    },
+    {
+      title: '更新日志',
+      path:
+        'https://github.com/form-render/schema-generator/blob/master/CHANGELOG.md',
+    },
+  ],
   outputPath: 'docs-dist',
   mode: 'site',
   base: '/schema-generator/',
